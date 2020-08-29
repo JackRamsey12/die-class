0675ad26906d493d8383e3ebe657efaea68f21c9
root <root@DESKTOP-D0KBG21.localdomain>
2020-08-28 21:44:12 -0400

    added main program file

---
+++ die-main.cc
@@ -0,0 +1,24 @@
+/*
+*   Author: Jack Ramsey
+*   Date: 8/28/20
+*   Description: this program simulates rolling a die.
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include "die.h"
+using namespace std;
+//function prototypes
+
+int main() {
+
+    Die d;
+    cout << "Face value: "<< d << endl;
+    Die d12(12);
+    cout << "Face value: "<< d12 << endl;
+    d12.setNumSides(24);
+    for (size_t i = 0; i < 6; i++) {
+        cout << "Roll: " << d12.roll() << endl;
+    }
+    return 0;
+}
